3c7110f88a12b17897a1b98de63076eca7fccfee
Matt Bailey <matt@matt-bailey.co.uk>
2025-10-01 11:29:37 +0100

    Extract text from PDF first to see effect on token usage and time

--- src/main/java/com/lydtechconsulting/pdfsummarizer/PdfSummarizerApplication.java
+++ src/main/java/com/lydtechconsulting/pdfsummarizer/PdfSummarizerApplication.java
@@ -1,5 +1,8 @@
 package com.lydtechconsulting.pdfsummarizer;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
@@ -13,6 +16,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class PdfSummarizerApplication implements CommandLineRunner {
@@ -29,9 +35,14 @@ public class PdfSummarizerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        // Send the PDF directly to Claude to summarise
         var pdfResource = new ClassPathResource("/Streambased-Reference-Architecture.pdf");
         System.out.println("Summary:\n" + this.summarize(new Media(Media.Format.DOC_PDF, pdfResource)));
 
+        // Extract the text from the PDF using PDFBox and then send the text to Claude to be summarised
+        var pdfFile = new ClassPathResource("/Streambased-Reference-Architecture.pdf").getFile();
+        System.out.println("Summary of extracted text:\n" + this.summarize(this.getTextFromPdf(pdfFile)));
     }
 
     @Bean
@@ -61,5 +72,23 @@ public class PdfSummarizerApplication implements CommandLineRunner {
                                 .media(file))
                 .call()
                 .content();
+    }
+
+    public String summarize(String text) {
+        return chatClient
+                .prompt()
+                .user(promptUserSpec ->
+                        promptUserSpec
+                                .text("Please summarise this text: " + text))
+                .call()
+                .content();
+    }
+
+    private String getTextFromPdf(File pdfFile) throws IOException {
+        PDDocument document = Loader.loadPDF(pdfFile);
+        PDFTextStripper stripper = new PDFTextStripper();
+        String text = stripper.getText(document);
+        document.close();
+        return text;
     }
 }
